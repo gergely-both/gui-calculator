2dd0774dcf920ada7935fc91a86d353e8b86aa22
Gergely Both <75031498+gergely-both@users.noreply.github.com>
2024-03-23 16:40:14 +0100

    slight restructuring

--- gui-calculator.py
+++ gui-calculator.py
@@ -71,10 +71,104 @@ class Calculator:
         self.memory = "0" # zero initial value
         self.history = [] # memory for repeating operations
 
+    def parse_input(self, value):
+        """dispatcher of int or str value type; special case: math pi"""
+        if type(value) == int:
+            self.input_number(value)
+        elif type(value) == str:
+            if value == "π":
+                self.input_number(math.pi)
+            else:
+                self.input_symbol(value)
+
+    def input_number(self, value):
+        if self.numeric_input:
+            if len(self.numeric_input) == 1 and self.numeric_input[0] == "0":
+                if value != "0":
+                    self.numeric_input[0] = str(value)
+                    self.label.config(text=self.numeric_input[0])
+            else:
+                self.numeric_input.append(str(value))
+                self.display_input()
+        else:
+            self.numeric_input.append(str(value))
+            self.label.config(text=self.numeric_input[0])
+
+    def input_symbol(self, value):
+        if value in self.opset:
+            self.input_operator(value)
+        elif value == ".":
+            self.dot_value()
+        elif value == "SIG":
+            self.swap_sign()
+        elif value == "BACK":
+            self.remove_last()
+        elif value == "C":
+            self.clear_user_input()
+        elif value == "AC":
+            self.clear_user_input()
+            self.query.clear()
+        elif value in {"m+", "m-"}:
+            self.memory_operate(value)
+        elif value == "mr":
+            self.memory_recall()
+        elif value == "√":
+            self.make_sqrt()
+        elif value == "^2":
+            self.make_squared()
+
+    def input_operator(self, operator):
+        previous_value = "".join(self.numeric_input)
+        self.numeric_input.clear()
+        if not self.query:
+            if operator == "=":
+                self.query.append(previous_value if previous_value else "0")
+            else:
+                self.query.extend([previous_value if previous_value else "0", operator])
+        elif len(self.query) == 1:
+            if operator == "=" and not self.history:
+                self.query[0] = previous_value if previous_value else self.query[0]
+            elif operator == "=" and len(self.history) == 3:
+                self.query.extend([self.history[1], self.history[2]])
+                self.calculate(operator)
+            else:
+                self.query.append(operator)
+        elif len(self.query) == 2:
+            if previous_value:
+                self.query.append(previous_value)
+                self.calculate(operator)
+            elif operator == "=" and not previous_value:
+                self.query.append(self.query[0])
+                self.calculate(operator)
+            elif operator != "=" and not previous_value:
+                self.query[-1] = operator
+
+    def calculate(self, operator):
+        self.history.clear()
+        self.history.extend(self.query)
+        unrounded = eval("".join(self.query))
+        total_raw = round(unrounded, 10)
+        total = int(total_raw) if int(total_raw) == total_raw else total_raw
+        self.query.clear()
+        if operator == "=":
+            self.query.append(str(total))
+        else:
+            self.query.extend([str(total), operator])
+        self.label.config(text=total)
+
+    def clear_user_input(self):
+        self.numeric_input.clear()
+        self.label.config(text=0)
+
+    def remove_last(self):
+        if self.numeric_input:
+            self.numeric_input.pop()
+            self.label.config(text="".join(self.numeric_input) or "0")
+        
     def display_input(self):
         self.label.config(text="".join(self.numeric_input))
 
-    def dotted_value(self):
+    def dot_value(self):
         if not self.numeric_input:
             self.numeric_input.extend(["0", "."]) 
             self.display_input()
@@ -82,7 +176,7 @@ class Calculator:
             self.numeric_input.append(".")
             self.display_input()
 
-    def change_sign(self):
+    def swap_sign(self):
         sign = "-"
         if self.numeric_input and (len(self.numeric_input) > 1 or self.numeric_input[0] != "0"):
             if self.numeric_input[0] == sign:
@@ -97,6 +191,15 @@ class Calculator:
                 self.query[0] = sign + self.query[0]
             self.label.config(text=self.query[0])
 
+    def memory_operate(self, value):
+        operation = "+" if value[1] == "+" else "-"
+        self.memory = str(eval(self.memory + operation + ("".join(self.numeric_input) or self.query[0])))
+        self.numeric_input.clear()
+  
+    def memory_recall(self):
+        self.numeric_input = [char for char in self.memory]
+        self.display_input()
+            
     def make_sqrt(self):
         if self.numeric_input:
             sqrt_result = math.sqrt(float("".join(self.numeric_input)))
@@ -120,110 +223,6 @@ class Calculator:
             squared_pruned = int(squared_result) if int(squared_result) == squared_result else squared_result
             self.query[0] = str(squared_pruned)
         self.label.config(text=squared_pruned)
-
-    def clear_user_input(self):
-        self.numeric_input.clear()
-        self.label.config(text=0)
-
-    def remove_last(self):
-        if self.numeric_input:
-            self.numeric_input.pop()
-            self.label.config(text="".join(self.numeric_input) or 0)
-
-    def memory_operate(self, value):
-        operation = value[1]
-        self.memory = str(eval(self.memory + operation + ("".join(self.numeric_input) or self.query[0])))
-        self.numeric_input.clear()
-  
-    def memory_recall(self):
-        self.numeric_input = [i for i in self.memory]
-        self.display_input()
-
-    def input_operator(self, operator):
-        previous_value = "".join(self.numeric_input)
-        self.numeric_input.clear()
-        if not self.query:
-            if operator == "=":
-                self.query.append(previous_value if previous_value else "0")
-            else:
-                self.query.extend([previous_value if previous_value else "0", operator])
-        elif len(self.query) == 1:
-            if operator == "=" and not self.history:
-                self.query[0] = previous_value if previous_value else self.query[0]
-            elif operator == "=" and len(self.history) == 3:
-                self.query.extend([self.history[1], self.history[2]])
-                self.equals_to(operator)
-            else:
-                self.query.append(operator)
-        elif len(self.query) == 2:
-            if previous_value:
-                self.query.append(previous_value)
-                self.equals_to(operator)
-            elif operator == "=" and not previous_value:
-                self.query.append(self.query[0])
-                self.equals_to(operator)
-            elif operator != "=" and not previous_value:
-                self.query[-1] = operator
-
-    def equals_to(self, operator):
-
-        self.history.clear()
-        self.history.extend(self.query)
-        
-        unrounded = eval("".join(self.query))
-        total = round(unrounded, 10)
-        total = int(total) if int(total) == total else total
-        self.query.clear()
-        if operator == "=":
-            self.query.append(str(total))
-        else:
-            self.query.extend([str(total), operator])
-        self.label.config(text=total)
-
-    def input_number(self, value):
-        if self.numeric_input:
-            if len(self.numeric_input) == 1 and self.numeric_input[0] == "0":
-                if value != "0":
-                    self.numeric_input[0] = str(value)
-                    self.label.config(text=self.numeric_input[0])
-            else:
-                self.numeric_input.append(str(value))
-                self.display_input()
-        else:
-            self.numeric_input.append(str(value))
-            self.label.config(text=self.numeric_input[0])
-
-    def input_symbol(self, value):
-        if value in self.opset:
-            self.input_operator(value)
-        elif value == ".":
-            self.dotted_value()
-        elif value == "SIG":
-            self.change_sign()
-        elif value == "BACK":
-            self.remove_last()
-        elif value == "C":
-            self.clear_user_input()
-        elif value == "AC":
-            self.clear_user_input()
-            self.query.clear()
-        elif value in {"m+", "m-"}:
-            self.memory_operate(value)
-        elif value == "mr":
-            self.memory_recall()
-        elif value == "√":
-            self.make_sqrt()
-        elif value == "^2":
-            self.make_squared()
-            
-    def parse_input(self, value):
-        if type(value) == int:
-            self.input_number(value)
-        elif type(value) == str:
-            if value == "π":
-                self.input_number(math.pi)
-            else:
-                self.input_symbol(value)
 
         
 root = tk.Tk()
